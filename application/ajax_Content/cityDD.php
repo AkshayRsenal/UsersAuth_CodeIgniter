7f6a80d441839ce6b3eeec395aab82759cd373f9
Akiro <akshayraul07@gmail.com>
2019-12-04 07:49:48 +0100

    ver0.2 Database: usermanagement.sql added

--- application/ajax_Content/cityDD.php
+++ application/ajax_Content/cityDD.php
@@ -1,6 +1,8 @@
 <?php
-	$stateIdVal = $_REQUEST["stateID"];
 
-	echo $stateIdVal;
+	// This file is not in use and no code is used from here
+	/*$stateIdVal = $_REQUEST["stateID"];
+
+	echo $stateIdVal;*/
 
 ?>
