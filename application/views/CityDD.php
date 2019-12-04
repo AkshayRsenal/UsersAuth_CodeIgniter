d29dddbb9ba7e15f5ae6d46871adc0e251199df4
Akiro <akshayraul07@gmail.com>
2019-12-04 08:06:58 +0100

    ver0.2 Dropdown city_Select code updated

--- application/views/CityDD.php
+++ application/views/CityDD.php
@@ -1,20 +1,4 @@
-<!-- <style type="text/css">
-		.form-control {
-		  display: block;
-		  width: 100%;
-		  height: calc(1.5em + 0.75rem + 15px);
-		  padding: 0.375rem 0.75rem;
-		  font-size: 1.875rem;
-		  font-weight: 400;
-		  line-height: 1.5;
-		  color: #5c6873;
-		  background-color: #fff;
-		  background-clip: padding-box;
-		  border: 1px solid #e4e7ea;
-		  border-radius: 0.25rem;
-		  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-		}
-</style> -->
+
 <div class="input-group-prepend">
 	<span class="input-group-text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
 </div>
